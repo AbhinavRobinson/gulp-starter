69a5cbd04cf16993391a9b30b5cf3185e64d27a6
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2018-12-30 08:09:18 +0530

    Minify CSS files in src/css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 let gulp = require('gulp');
 let browserSync = require('browser-sync');
 let sass = require('gulp-sass');
+let cleanCSS = require('gulp-clean-css');
+let rename = require('gulp-rename');
 
 gulp.task('sync',function(){
     browserSync.init({
@@ -12,7 +14,24 @@ gulp.task('sync',function(){
 });
 
 gulp.task('sass',function(){
-    return gulp.src('app/scss/*.scss')
+    let sassInit = function(){
+        return gulp.src('app/scss/*.scss')
         .pipe(sass().on('error',sass.logError))
         .pipe(gulp.dest('src/css'));
+    }
+    gulp.watch('app/scss/*.scss',sassInit);
+});
+
+gulp.task('minify', function () {
+    let minifyInit = function(){
+        return gulp.src(["src/css/*.css","!src/css/*.min.css"])
+            .pipe(cleanCSS())
+            .pipe(rename(
+                {
+                    suffix: '.min'
+                }
+            ))
+            .pipe(gulp.dest('src/css'));
+    }
+    gulp.watch("src/css/*.css").on('change', minifyInit);
 });
