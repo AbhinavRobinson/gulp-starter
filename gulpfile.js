57f5c740dc9988a75c886c77caafecf6b22bcefd
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2018-12-30 07:34:45 +0530

    SASS compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync');
+let gulp = require('gulp');
+let browserSync = require('browser-sync');
+let sass = require('gulp-sass');
 
 gulp.task('sync',function(){
     browserSync.init({
@@ -8,4 +9,10 @@ gulp.task('sync',function(){
         }
     });
     gulp.watch(["**/*.html","**/*.js","**/*.css"]).on('change',browserSync.reload);
+});
+
+gulp.task('sass',function(){
+    return gulp.src('app/scss/*.scss')
+        .pipe(sass().on('error',sass.logError))
+        .pipe(gulp.dest('src/css'));
 });
